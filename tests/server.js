87b5809b0c6200a95eb54434e1eeec56fcd9ffee
Brandon Cannaday <brandon@paranoidferret.com>
2013-01-15 15:02:21 -0500

    Basic working uploader.

---
+++ tests/server.js
@@ -0,0 +1,35 @@
+var express = require('express'),
+    app = express(),
+    chunkLoader = require(__dirname + '/../lib/chunk-loader'),
+    client = chunkLoader.Client,
+    server = chunkLoader.Server(app);
+
+app.listen(8080);
+
+server.on('complete', function(file) {
+  console.log('Server complete: ' + file.tag.projectId);
+});
+
+server.auth = function(file, callback) {
+  callback();
+};
+
+setTimeout(function() {
+  client.uploadFile(__dirname + '/test-file.txt', { tag: { projectId: '4563' }}, function(err, client) {
+    client.on('error', function(err) {
+      console.log(err);
+    });
+
+    client.on('begin', function() {
+      console.log('on begin')
+    });
+
+    client.on('complete', function() {
+      console.log('complete');
+    });
+
+    client.on('progress', function(p) {
+      console.log('progress:' + ((p.sent / p.total) * 100));
+    });
+  });
+}, 1000);
